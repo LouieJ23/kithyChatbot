b9e783d9816f3ab5f2a90cb715de192484b996bd
LouieJay <louiejaysalvana@gmail.com>
2021-10-09 19:50:29 +0800

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+import request from "request";
 
 let postWebhook = (req, res) => {
     // Parse the request body from the POST
@@ -141,7 +141,20 @@ function handleMessage(sender_psid, received_message) {
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+    let response;
 
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+}
 }
 
 // Sends response messages via the Send API
