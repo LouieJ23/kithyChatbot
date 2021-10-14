90335eb8ae214957e1065767365f3fd80fe85a1c
LouieJ23 <louiejaysalvana@gmail.com>
2021-10-15 00:38:53 +0800

    update

--- src/server.js
+++ src/server.js
@@ -17,6 +17,8 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+'use strict';
+
 
 app.post('/webhook', (request, response) =>{
     dialogflowfulfillment(request, response)
