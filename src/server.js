e3e12a337eef6807d77cd2c9b878bd51d54a05e0
LouieJ23 <louiejaysalvana@gmail.com>
2021-10-14 11:30:09 +0800

    update

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/homepage", function(req,res) {
+app.get("/", function(req,res) {
         res.send('<h1> This is my web app');
     });
 
