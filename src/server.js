c6f39da062bcf6b7246caacdeba98cfa5ae1ac91
LouieJ23 <louiejaysalvana@gmail.com>
2021-10-23 12:36:52 +0800

    update

--- src/server.js
+++ src/server.js
@@ -35,8 +35,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //      const _agent = new WebhookClient({request:request, response:response});
     app.post("/webhook", function(request, response) {
 
-        const fulfillment = request.body.queryResult.fulfillmentMessages;
-       const obj = {fulfillment};
+        //const fulfillment = request.body.queryResult.fulfillmentText;
+       const fulfillment = request.fulfillmentMessages[0].text.text[0];
+        const obj = {fulfillment};
         console.log("json string is" + JSON.stringify(obj));
         response.send(JSON.stringify(obj));
     })
