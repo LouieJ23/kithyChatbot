4ca64f0c7d9f4eec83bd89a7051cbe6fd4602f71
LouieJ23 <louiejaysalvana@gmail.com>
2021-10-24 20:44:23 +0800

    update

--- src/server.js
+++ src/server.js
@@ -14,28 +14,28 @@ const app = express().use(bodyParser.json())
 //let app = express();
 
 
-
-//config view engine
-viewEngine(app);
-
-//use body-parser to post data
-app.use(bodyParser.json());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//'use strict';
-
-
-        app.post("/webhook", function(request, response) {
-        // let _agent = new WebhookClient({request,response});
-        //const fulfillment = request.body.queryResult.fulfillmentText;
-        const fulfillment = request.body.queryResult.fulfillmentMessages[0].text.text[0];
-        const intent = request.body.queryResult.intent.displayName;
-        const obj = {fulfillment};
-        console.log("json string is" + JSON.stringify(obj) + "from:" + intent);
-        response.send(JSON.stringify(obj));
-
-        });
+//
+// //config view engine
+// viewEngine(app);
+//
+// //use body-parser to post data
+// app.use(bodyParser.json());
+// app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+//
+// //'use strict';
+//
+//         //this will fetch the dialogflow intents and response
+//         app.post("/webhook", function(request, response) {
+//         // let _agent = new WebhookClient({request,response});
+//         //const fulfillment = request.body.queryResult.fulfillmentText;
+//         const fulfillment = request.body.queryResult.fulfillmentMessages[0].text.text[0];
+//         const intent = request.body.queryResult.intent.displayName;
+//         const obj = {fulfillment};
+//         console.log("json string is" + JSON.stringify(obj) + "from:" + intent);
+//         response.send(JSON.stringify(obj));
+//
+//         });
                 //init all web routes
                 initWebRoute(app);
                 let port = process.env.PORT||8080;
