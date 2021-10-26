969d5693a6d1bfaa2ac0b8eda989add519f9b20b
LouieJ23 <louiejaysalvana@gmail.com>
2021-10-26 15:17:04 +0800

    update

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-//import {dialogflow} from "actions-on-google";
-
 require("dotenv").config();
 import express from "express";
 import viewEngine from "./config/viewEngine";
@@ -20,17 +18,17 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-        //this code will get the intent triggered in dialogflow through json
-        app.post("/webhook", function(request, response) {
-        // let _agent = new WebhookClient({request,response});
-        //const fulfillment = request.body.queryResult.fulfillmentText;
-        const fulfillment = request.body.queryResult.fulfillmentMessages[0].text.text[0];
-        const obj = {fulfillment};
-        console.log("json string is" + JSON.stringify(obj));
-        response.send(JSON.stringify(obj));
-
-});
+//
+//         //this code will get the intent triggered in dialogflow through json
+//         app.post("/webhook", function(request, response) {
+//         // let _agent = new WebhookClient({request,response});
+//         //const fulfillment = request.body.queryResult.fulfillmentText;
+//         const fulfillment = request.body.queryResult.fulfillmentMessages[0].text.text[0];
+//         const obj = {fulfillment};
+//         console.log("json string is" + JSON.stringify(obj));
+//         response.send(JSON.stringify(obj));
+//
+// });
 
         //init all web routes
         initWebRoute(app);
