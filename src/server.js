959523d54aaee31c8e23317cc09030a04d2d2300
LouieJ23 <louiejaysalvana@gmail.com>
2021-10-27 23:21:44 +0800

    update

--- src/server.js
+++ src/server.js
@@ -38,8 +38,10 @@ app.post("/webhook", (req, res) => {
 
                 if(input === "What is your mobile phone contact?")
                         agent.add('The contact number is: 09555555555');
-                        console.log("This is the input: ", input);
-                        console.log("This is the response: ", agent.responseMessages_[0].text[0].text[0]);
+                        console.log("This is the input: "+ input);
+                        const fulfillment = request.body.queryResult.fulfillmentMessages[0].text.text[0];
+                                const obj = {fulfillment};
+                                console.log("json string is" + JSON.stringify(obj));
         }
 
         function fallback(agent){
