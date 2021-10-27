1748432478e6c0f7841de4dc5e8fa4bc834bb4aa
LouieJ23 <louiejaysalvana@gmail.com>
2021-10-28 00:00:19 +0800

    update

--- src/server.js
+++ src/server.js
@@ -42,15 +42,18 @@ app.post("/webhook", (req, res) => {
                 if(input === "What is your mobile phone contact?")
                         agent.add('The contact number is: 09555555555');
                        console.log("This is the input: "+ input);
-                        const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
-                                const obj = {fulfillment};
-                                console.log("json string is" + JSON.stringify(obj));
-
         }
 
+        function fallback (){
+                const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
+                const obj = {fulfillment};
+                console.log("json string is" + JSON.stringify(obj));
+                res.send(JSON.stringify(obj));
+        }
         let intentMap = new Map();
         intentMap.set('Default Welcome Intent', welcomeIntent);
         intentMap.set('Contact Information', contact);
+        intentMap.set('Default Fallback Intent', fallback);
         _agent.handleRequest(intentMap);
         });
 
