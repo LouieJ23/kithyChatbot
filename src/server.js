b4177dadef9d486007fcf73b3b8759d61d9a6689
LouieJ23 <louiejaysalvana@gmail.com>
2021-10-27 19:30:49 +0800

    update

--- src/server.js
+++ src/server.js
@@ -9,26 +9,25 @@ const app = express().use(bodyParser.json())
 
 
 
-
-//config view engine
-viewEngine(app);
-
 //use body-parser to post data
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//
-//         //this code will get the intent triggered in dialogflow through json
-//         app.post("/webhook", function(request, response) {
-//         // let _agent = new WebhookClient({request,response});
-//         //const fulfillment = request.body.queryResult.fulfillmentText;
-//         const fulfillment = request.body.queryResult.fulfillmentMessages[0].text.text[0];
-//         const obj = {fulfillment};
-//         console.log("json string is" + JSON.stringify(obj));
-//         response.send(JSON.stringify(obj));
-//
-// });
+//config view engine
+viewEngine(app);
+
+
+        //this code will get the intent triggered in dialogflow through json
+        app.post("/webhook", function(request, response) {
+        // let _agent = new WebhookClient({request,response});
+        //const fulfillment = request.body.queryResult.fulfillmentText;
+                const fulfillment = request.body.queryResult.fulfillmentMessages[0].text.text[0];
+                const obj = {fulfillment};
+                console.log("json string is" + JSON.stringify(obj));
+                response.send(JSON.stringify(obj));
+
+        });
 
         //init all web routes
         initWebRoute(app);
