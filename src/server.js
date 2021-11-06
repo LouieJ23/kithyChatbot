fe21139445aba646055e1756bb4169d9c345f737
LouieJ23 <louiejaysalvana@gmail.com>
2021-11-07 02:08:56 +0800

    update

--- src/server.js
+++ src/server.js
@@ -81,29 +81,29 @@ app.post("/webhook", (req, res) => {
         // });
 
         app.get((req, res) => {
-                // // Your verify token. Should be a random string.
-                // let VERIFY_TOKEN = "FACEBOOK_PAGE_ACCESS_TOKEN"
+                // Your verify token. Should be a random string.
+                let VERIFY_TOKEN = "FACEBOOK_PAGE_ACCESS_TOKEN"
             
-                // // Parse the query params
-                // let mode = req.query['hub.mode'];
-                // let token = req.query['hub.verify_token'];
-                // let challenge = req.query['hub.challenge'];
+                // Parse the query params
+                let mode = req.query['hub.mode'];
+                let token = req.query['hub.verify_token'];
+                let challenge = req.query['hub.challenge'];
             
-                // // Checks if a token and mode is in the query string of the request
-                // if (mode && token) {
+                // Checks if a token and mode is in the query string of the request
+                if (mode && token) {
             
-                //     // Checks the mode and token sent is correct
-                //     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+                    // Checks the mode and token sent is correct
+                    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
             
-                //         // Responds with the challenge token from the request
-                //         console.log('WEBHOOK_VERIFIED');
-                //         res.status(200).send(challenge);
+                        // Responds with the challenge token from the request
+                        console.log('WEBHOOK_VERIFIED');
+                        res.status(200).send(challenge);
             
-                //     } else {
-                //         // Responds with '403 Forbidden' if verify tokens do not match
-                //         res.sendStatus(403);
-                //     }
-                // }
+                    } else {
+                        // Responds with '403 Forbidden' if verify tokens do not match
+                        res.sendStatus(403);
+                    }
+                }
 
                 console.log('This is the response: '+res);
             });
