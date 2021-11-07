d6eb7e151294b9bc4f9abbd3662cb05b48dbd227
LouieJ23 <louiejaysalvana@gmail.com>
2021-11-07 23:48:21 +0800

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -8,8 +8,8 @@ let router = express.Router();
 
 let initWebRoute = (app)=> {
     router.get("/", homepageController.getHomepage);
-    router.get("/webhook", chatbotController.getWebhook);
     router.post("/webhook", chatbotController.postWebhook);
+    router.get("/webhook", chatbotController.getWebhook);
     return app.use("/", router);
 };
 
