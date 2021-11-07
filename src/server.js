0fa6740940ff14be7245e5603fe77f254de5db09
LouieJ23 <louiejaysalvana@gmail.com>
2021-11-07 18:57:09 +0800

    update

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import request from "request";
 import express from "express";
 import viewEngine from "./config/viewEngine";
 import initWebRoute from "./routes/web.js";
