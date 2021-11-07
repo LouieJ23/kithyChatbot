11f999438c256b78176be38aaed1800420f14bd3
LouieJ23 <louiejaysalvana@gmail.com>
2021-11-07 15:05:01 +0800

    update

--- src/server.js
+++ src/server.js
@@ -26,33 +26,10 @@ viewEngine(app);
         // });
  //init all web routes
  initWebRoute(app);
-       
- let postWebhook=(req, res) => {
+     
+app.post("/webhook", (req, res) => {
         let _agent = new WebhookClient({request: req, response:res});
-        // Parse the request body from the POST
-        let body = req.body;
 
-        // Check the webhook event is from a Page subscription
-        if (body.object === 'page') {
-
-        // Iterate over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
-
-        // Gets the body of the webhook event
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-
-        });
-
-         // Return a '200 OK' response to all events
-         res.status(200).send('EVENT_RECEIVED');
-
-        } else {
-            // Return a '404 Not Found' if event is not from a page subscription
-            res.sendStatus(404);
-        }
-   
-    
 
         function welcomeIntent(agent) {
                 const input = req.body.queryResult.queryText;
@@ -102,65 +79,8 @@ viewEngine(app);
         intentMap.set('Contact Information', contact);
         intentMap.set('Default Fallback Intent', fallback);
         _agent.handleRequest(intentMap);
-};
-     
-// app.post("/webhook", (req, res) => {
-//         let _agent = new WebhookClient({request: req, response:res});
+        });
 
-
-//         function welcomeIntent(agent) {
-//                 const input = req.body.queryResult.queryText;
-
-//                 if(input === "Just going to say hi")
-//                 {
-//                         agent.add("Hello there, how can I help you Louie?");
-//                         console.log("This is the input: "+ input);
-//                         const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
-//                         const obj = {fulfillment};
-//                         console.log("json string is" + JSON.stringify(obj));
-//                 }
-//                 else {
-//                         agent.add("Hello there, how can I help you man?");
-//                         console.log("This is the input: "+ input);
-//                         const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
-//                         const obj = {fulfillment};
-//                         console.log("json string is" + JSON.stringify(obj));
-//                 }
-//         }
-//         function contact(agent) {
-//                const input = req.body.queryResult.queryText;
-//                console.log("This is the input: " + input);
-//                 if(input === "What is your mobile phone contact?") {
-//                         agent.add('The contact number is: 09555555555');
-//                         console.log("This is the response: " + res);
-//                 }
-//                        else if (input !== "What is your mobile phone contact?")
-//                 {
-//                                 console.log("This is the input: " + input);
-//                                 const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
-//                                 const obj = {fulfillment};
-//                                 console.log("json string is" + JSON.stringify(obj));
-//                                 res.send(JSON.stringify(obj));
-
-//                 }
-//         }
-
-//         function fallback (){
-//                 const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
-//                 const obj = {fulfillment};
-//                 console.log("json string is" + JSON.stringify(obj));
-//                 res.send(JSON.stringify(obj));
-//         }
-//         let intentMap = new Map();
-//         intentMap.set('Default Welcome Intent', welcomeIntent);
-//         intentMap.set('Contact Information', contact);
-//         intentMap.set('Default Fallback Intent', fallback);
-//         _agent.handleRequest(intentMap);
-//         });
-
-        // app.get((req, res)=>{
-        //         console.log(res);
-        // });
 
         app.get((req, res) => {
                 // Your verify token. Should be a random string.
