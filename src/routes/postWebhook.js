1a3568d30ca202895a300654dade734704009e4d
LouieJ23 <louiejaysalvana@gmail.com>
2021-11-13 19:58:39 +0800

    update

--- src/routes/postWebhook.js
+++ src/routes/postWebhook.js
@@ -10,57 +10,57 @@ router.get("/", (req, res)=>{
         });
 
 router.post("/", (req, res)=>{
-            let _agent = new WebhookClient({request: req, response:res});
+            // let _agent = new WebhookClient({request: req, response:res});
     
     
-            function welcomeIntent(agent) {
-                    const input = req.body.queryResult.queryText;
+            // function welcomeIntent(agent) {
+            //         const input = req.body.queryResult.queryText;
     
-                    if(input === "Just going to say hi")
-                    {
-                            agent.add("Hello there, how can I help you Louie?");
-                            console.log("This is the input: "+ input);
-                            const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
-                            const obj = {fulfillment};
-                            console.log("json string is" + JSON.stringify(obj));
-                    }
-                    else {
-                            agent.add("Hello there, how can I help you man?");
-                            console.log("This is the input: "+ input);
-                            const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
-                            const obj = {fulfillment};
-                            console.log("json string is" + JSON.stringify(obj));
-                    }
-            }
-            function contact(agent) {
-                   const input = req.body.queryResult.queryText;
-                   console.log("This is the input: " + input);
-                    if(input === "What is your mobile phone contact?") {
-                            agent.add('The contact number is: 09555555555');
-                            console.log("This is the response: " + res);
-                    }
-                           else if (input !== "What is your mobile phone contact?")
-                    {
-                                    console.log("This is the input: " + input);
-                                    const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
-                                    const obj = {fulfillment};
-                                    console.log("json string is" + JSON.stringify(obj));
-                                    res.send(JSON.stringify(obj));
+            //         if(input === "Just going to say hi")
+            //         {
+            //                 agent.add("Hello there, how can I help you Louie?");
+            //                 console.log("This is the input: "+ input);
+            //                 const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
+            //                 const obj = {fulfillment};
+            //                 console.log("json string is" + JSON.stringify(obj));
+            //         }
+            //         else {
+            //                 agent.add("Hello there, how can I help you man?");
+            //                 console.log("This is the input: "+ input);
+            //                 const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
+            //                 const obj = {fulfillment};
+            //                 console.log("json string is" + JSON.stringify(obj));
+            //         }
+            // }
+            // function contact(agent) {
+            //        const input = req.body.queryResult.queryText;
+            //        console.log("This is the input: " + input);
+            //         if(input === "What is your mobile phone contact?") {
+            //                 agent.add('The contact number is: 09555555555');
+            //                 console.log("This is the response: " + res);
+            //         }
+            //                else if (input !== "What is your mobile phone contact?")
+            //         {
+            //                         console.log("This is the input: " + input);
+            //                         const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
+            //                         const obj = {fulfillment};
+            //                         console.log("json string is" + JSON.stringify(obj));
+            //                         res.send(JSON.stringify(obj));
     
-                    }
-            }
+            //         }
+            // }
     
-            function fallback (){
-                    const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
-                    const obj = {fulfillment};
-                    console.log("json string is" + JSON.stringify(obj));
-                    res.send(JSON.stringify(obj));
-            }
-            let intentMap = new Map();
-            intentMap.set('Default Welcome Intent', welcomeIntent);
-            intentMap.set('Contact Information', contact);
-            intentMap.set('Default Fallback Intent', fallback);
-            _agent.handleRequest(intentMap);
+            // function fallback (){
+            //         const fulfillment = req.body.queryResult.fulfillmentMessages[0].text.text[0];
+            //         const obj = {fulfillment};
+            //         console.log("json string is" + JSON.stringify(obj));
+            //         res.send(JSON.stringify(obj));
+            // }
+            // let intentMap = new Map();
+            // intentMap.set('Default Welcome Intent', welcomeIntent);
+            // intentMap.set('Contact Information', contact);
+            // intentMap.set('Default Fallback Intent', fallback);
+            // _agent.handleRequest(intentMap);
             
 
             console.log(req.body);
